9e94c482555cbf70bec899c86a4442d5af49aa36
iconstudio <yoyofa2@hotmail.com>
2017-10-05 18:09:57 +0900

    Core file Added.
    Signed-off-by: iconstudio <yoyofa2@hotmail.com>

---
+++ stream.py
@@ -0,0 +1,38 @@
+
+import os
+from pico2d import *
+
+false = False
+true = True
+
+class null:
+    def __bool__(self):
+        return false
+
+    def __abs__(self):
+        return 0
+
+    def __sizeof__(self):
+        return 0
+
+class gravitons:
+    name = "None"
+
+    depth = 0
+    x, y = 0, 0
+    gravity = 0
+    sprite = null
+
+    def __init__(self, ndepth = 0, nx = 0, ny = 0):
+        self.depth = ndepth
+        self.x, self.y = nx, ny
+
+    def __str__(self):
+        return self.name
+
+open_canvas()
+show_cursor()
+
+
+#grass = load_image('grass.png')
+#character = load_image('run_animation.png')
