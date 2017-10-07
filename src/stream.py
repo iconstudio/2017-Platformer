e39fdc8151bf69bf02981037dad146f8116aea31
iconstudio <yoyofa2@hotmail.com>
2017-10-07 20:45:59 +0900

    Test
    Signed-off-by: iconstudio <yoyofa2@hotmail.com>

--- src/stream.py
+++ src/stream.py
@@ -54,15 +54,44 @@ class game:
         open_canvas(self.width, self.height, true)
         show_cursor()
 
+# Object : Sprites
+class sprite():
+    number = 0
+
+    def __init__(self, filepath, number):
+        self.__data__ = load_image(filepath)
+
+        # the number of sprite in a image
+        self.number = number
+        # size of each index
+        try:
+            self.width = self.__data__.w / number
+            self.height = self.__data__.h / number
+
+            tempTy = type(number)
+            if tempTy != int and tempTy != float:
+                raise RuntimeError("스프라이트 불러오기 시 인자가 숫자가 아닙니다.")
+        except ZeroDivisionError:
+            raise RuntimeError("스프라이트의 갯수는 0개가 될 수 없습니다.")
+
+    def draw(self, index, x, y, xscale = int(1), yscale = int(1), rot = float(0.0)):
+        if rot != 0.0: # pico2d does not support scaling + rotating draw.
+            self.__data__.rotate_draw(rot, x, y, self.width * xscale, self.height * yscale)
+        else:
+            self.__data__.clip_draw(index * self.width, 0, self.width, self.height, x, y, self.width * xscale, self.height * yscale)
+
 # Object : Gravitons
-class graviton:
+class graviton(object):
     name = "None"
-    sprite = None
+
+    sprite_index = None
+    image_index = float(0)
+    image_speed = float(0)
+
     visible = true
-
     depth = 0
-    x, y = 0, 0
 
+    x, y = 0, 0
     gravity_current = 0
     gravity = 0
 
@@ -73,19 +102,38 @@ class graviton:
     def __str__(self):
         return self.name
 
+    def draw_self(self):
+        if (self.sprite_index != None and type(self.sprite_index) == sprite):
+            draw_sprite(self.sprite_index, self.image_index, self.x, self.y)
+
     def event_step(self):
         pass
 
     def event_draw(self):
-        pass
+        self.draw_self()
 
 # Object : Functions
+def sprite_load(filepath, name = str("default"), number = int(1)):
+    new = sprite(filepath, number)
+    sprite_list[name] = new
+    return new
+
+def draw_sprite(sprite, index, x = int(0), y = int(0), xscale = int(1), yscale = int(1), rot = float(0.0)):
+    sprite.draw(index, x, y, xscale, yscale, rot)
+
 def instance_create(Ty, depth = int(0), x = int(0), y = int(0)):
-    return Ty(depth, x, y)
+    tempObj = Ty(depth, x, y)
+    instance_list.append(tempObj)
+
+    return tempObj
 
 # Main : Game Settings
 Game = game()
 Game.begin()
+
+test = sprite_load("test.png", "ball", 1)
+testo = instance_create(graviton)
+testo.sprite_index = test
 
 # Event : Global
 def event_global():
@@ -106,10 +154,11 @@ while running:
     if not event_global():
         break
 
-    if instance_list.__sizeof__() > 0:
+    if len(instance_list) > 0:
+        #print(instance_list.__len__())
         instance_iter_update()
         for inst in instance_iterator:
-            inst.sprite.event_step()
+            inst.event_step()
 
         for inst in instance_iterator:
             inst.event_draw()
