4e233d42143875b2140d34795a784a34244d18c4
iconstudio <yoyofa2@hotmail.com>
2017-11-06 13:47:23 +0900

    IO module modified.
    Signed-off-by: iconstudio <yoyofa2@hotmail.com>

--- src/module/constants.py
+++ src/module/constants.py
@@ -2,7 +2,7 @@ import sdl2.pixels as px
 
 __all__ = [
     "false", "true", "screen_width", "screen_height", "screen_scale",
-    "path_resource", "path_image", "path_font", "path_theme", "path_entity",
+    "path_resource", "path_image", "path_font", "path_theme", "path_entity", "path_background"
     "c_white"
 ]
 
@@ -18,6 +18,7 @@ path_image = path_resource + "img\\"
 path_font = path_resource + "font\\"
 path_theme = path_image + "theme\\"
 path_entity = path_image + "entity\\"
+path_background = path_image + "bg\\"
 
 # Colors
 c_white = px.SDL_Color(255, 255, 255)
--- src/module/framework.py
+++ src/module/framework.py
@@ -2,9 +2,79 @@ import sdl2.keyboard as keyboard
 from module.pico2d import *
 
 __all__ = [
-    "GameState", "change_state", "push_state", "pop_state", "quit", "run"
+    "GameState", "change_state", "push_state", "pop_state", "quit", "run",
+	"io",
 ] + keyboard.__all__
 
+
+# Object : IO procedure
+class oIOProc:
+    checking = {}
+    class iochecker:
+        code = None
+        gravity = 0
+        life = 3
+        owner = None
+
+        def __init__(self, nowner, key: SDL_Keycode):
+            self.owner = nowner
+            self.code = key
+
+        def event_step(self):
+            if self.life <= 0:
+                global io
+                io.checking[self.code] = None
+                self.owner.check_pressed = false
+                del self
+            else:
+                self.life -= 1
+
+    class ionode:
+        code = None
+        check: bool = false
+        check_pressed: bool = false
+
+        def __init__(self, key: SDL_Keycode):
+            self.code = key
+
+    # This dictionary contains only the Codes of keyboard.
+    key_list = {}
+
+    def key_add(self, key: SDL_Keycode):
+        newnode = self.ionode(key)
+        self.key_list[key] = newnode
+        return newnode
+
+    def key_check(self, key: SDL_Keycode) -> bool:
+        try:
+            return (self.key_list[key]).check
+        except KeyError:
+            return false
+
+    def key_check_pressed(self, key: SDL_Keycode) -> bool:
+        try:
+            return (self.key_list[key]).check_pressed
+        except KeyError:
+            return false
+
+    def proceed(self, kevent):
+        try:
+            node = self.key_list[kevent.key]
+
+            if kevent.type == SDL_KEYDOWN:
+                node.check = true
+                node.check_pressed = true
+                if self.checking[node.code] == None:
+                    self.checking[node.code] = self.iochecker(node, node.code)
+                # print(true)
+            elif kevent.type == SDL_KEYUP:
+                node.check = false
+                node.check_pressed = false
+                # print(false)
+        except KeyError:
+            return
+
+io = oIOProc()
 
 class GameState:
     def __init__(self, state):
@@ -66,6 +136,7 @@ def run(start_state):
         stack[-1].handle_events()
         stack[-1].update()
         stack[-1].draw()
+        
     # repeatedly delete the top of the stack
     while len(stack) > 0:
         stack[-1].exit()
