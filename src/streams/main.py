390af9f1742bc744baedb3600e0df9943a8ea39c
iconstudio <yoyofa2@hotmail.com>
2017-11-16 06:20:14 +0900

    Main menu modified.
    Signed-off-by: iconstudio <yoyofa2@hotmail.com>

--- src/streams/main.py
+++ src/streams/main.py
@@ -18,8 +18,10 @@ __all__ = [
 name = "main_state"
 menusel = None
 menudic = {}
+
 menusy = screen_height - 180
 mpush = 0
+
 
 class MenuNode:
     captioon: str = "submenu"
@@ -76,6 +78,79 @@ def menu_create(ncaption) -> Menu:
 menu_begin: Menu = None
 
 
+class MainMenu:
+    maxdepth = 0
+    menu_hsize = 50
+    menucnt = []  # The number of menu nodes
+    menusel = []  # Current selection
+    menuold = []  # Previous selection
+    menuscl = []
+    menupos = []  # The initiative y position of a menu node
+
+    def menu_clear(self, index: int, default_selected: int = 9):
+        if (index > self.maxdepth):
+            self.maxdepth = index
+        self.menucnt[index] = 0
+        self.menusel[index] = default_selected
+        self.menuold[index] = default_selected
+        self.menuscl[index] = 0
+        self.menupos[index] = self.menu_hsize * default_selected
+
+    def update(self, frame_time):
+        for i in range(self.maxdepth):
+            self.menupos[i] += (-self.menu_hsize * self.menusel[i] - self.menupos[i]) / 5 * (
+                frame_time * delta_velocity(10))
+            self.menuscl[i] -= self.menuscl[i] / 5 * (frame_time * delta_velocity(10))
+
+    def draw(self, frame_time):
+        """
+
+for (j = 0; j <= maxdepth; j += 1) {
+ dy = menupos[j] - 10;
+ dalpha = 1 - abs(menurot / 90 - j);
+ for (i = 0; i < menucnt[j]; i += 1) {
+  if (i = menusel[j])
+   draw_set_alpha(aalpha);
+  else
+   draw_set_alpha(dalpha * aalpha);
+  if (i = menusel[j])
+   dscl = 4 - menuscl[j] * 2;
+  else if (i = menuold[j])
+   dscl = 2 + menuscl[j] * 2;
+  else
+   dscl = 2;
+  draw_set_color(make_color_hsv(0, 0, 255 * menucol[j, i]));
+  sw = menu_text(8 + rpush[2], dy, menucap[j, i], dscl / 6, menurot - j * 90);
+  ldy = dy;
+  dy += 5 * dscl + 1.5;
+  if (menuinf[j, i] != "") {
+   if (i = menusel[j]) {
+    draw_set_alpha(aalpha * infoscl[j]);
+    menu_text(8 + rpush[2], dy - 0.875 * infoscl[j], "- " + menuinf[j, i], infoscl[j] * 1.75 / 6, menurot - j * 90);
+    dy += 10.5 * infoscl[j] + 1;
+   } else if (i = menuold[j]) {
+    draw_set_alpha(aalpha * infosco[j]);
+    menu_text(8 + rpush[2], dy - 0.875 * infosco[j], "- " + menuinf[j, i], infosco[j] * 1.75 / 6, menurot - j * 90);
+    dy += 10.5 * infosco[j] + 1.5;
+   }
+  }
+  if (menuvar[j, i] != "0") {
+   draw_set_alpha(dalpha * aalpha);
+   draw_set_color(make_color_hsv(0, 0, 176 * menucol[j, i]));;
+   d3d_transform_set_rotation_y(90);
+   d3d_transform_add_translation(-sw, 0, -sw);
+   d3d_transform_add_rotation_y(menurot - j * 90);
+   d3d_transform_add_translation(sw, 0, sw);
+   draw_text_transformed(8 + (string_width(menucap[j, i]) + 3) * dscl / 6 + rpush[2], ldy, string(variable_global_get(menuvar[j, i])), dscl / 6, dscl / 6, 0);
+   d3d_transform_set_identity();
+   draw_set_color(make_color_hsv(0, 0, 255 * menucol[j, i]));
+  }
+ }
+}
+        """
+        pass
+
+
 # noinspection PyGlobalUndefined
 def enter():
     io.key_add(SDLK_UP)
@@ -110,7 +185,7 @@ def exit():
 def update(frame_time):
     global mpush
     if mpush != 0:
-        mpush -= mpush / 4 * (frame_time * delta_velocity(5))
+        mpush -= mpush / 4 * (frame_time * delta_velocity(10))
 
     global menusel, menu_begin, menu_opt, menu_credit, menu_exit
     if io.key_check_pressed(SDLK_z):
@@ -134,7 +209,7 @@ def update(frame_time):
             menusel = menusel.nbefore
             if menusel == None:
                 menusel = Menu.menu_last
-                mpush = -200
+                mpush = 200
             else:
                 mpush = -50
     elif io.key_check_pressed(SDLK_DOWN):
@@ -142,7 +217,7 @@ def update(frame_time):
             menusel = next(menusel)
             if menusel == None:
                 menusel = menu_begin
-                mpush = 200
+                mpush = -200
             else:
                 mpush = 50
 
