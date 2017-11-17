c76d02afd21d147560da9ceec071a6d0b6676eb9
iconstudio <yoyofa2@hotmail.com>
2017-11-18 00:37:01 +0900

    Many Resources added.
    Signed-off-by: iconstudio <yoyofa2@hotmail.com>

---
+++ res/img/theme/vexodus_tileset.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="vexodus_tileset" tilewidth="20" tileheight="20" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="20" height="20" source="brick_castle_0.png"/>
+ </tile>
+ <tile id="4">
+  <image width="20" height="20" source="stonewall_0.png"/>
+ </tile>
+ <tile id="7">
+  <image width="20" height="20" source="metalic_black_0.png"/>
+ </tile>
+ <tile id="8">
+  <image width="20" height="20" source="BlockMetal_0.png"/>
+ </tile>
+ <tile id="9">
+  <image width="20" height="20" source="Block_0.png"/>
+ </tile>
+ <tile id="10">
+  <image width="20" height="20" source="BlockBlue_0.png"/>
+ </tile>
+ <tile id="17">
+  <image width="20" height="20" source="sLush_0.png"/>
+ </tile>
+ <tile id="20">
+  <image width="20" height="20" source="sLushFlat_0.png"/>
+ </tile>
+ <tile id="26">
+  <image width="20" height="20" source="brick_mine_0.png"/>
+ </tile>
+ <tile id="27">
+  <image width="20" height="20" source="door_0.png"/>
+ </tile>
+</tileset>
